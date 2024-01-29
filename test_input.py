10717249daa4a60678d9001973b9d6164eabd9c7
KenzoJ <148596766+KenzoJ@users.noreply.github.com>
2024-01-29 00:55:17 +0000

    Almost done with input testing before adding to Discord bot

--- test_input.py
+++ test_input.py
@@ -1,36 +1,33 @@
+import requests
+
 dict = {}
 nature = [
   "Bashful", "Docile", "Hardy", "Quirky", "Serious", "Adamant", "Brave", "Lonely", "Naughty", "Bold", "Impish", "Lax", "Relaxed", "Modest", "Mild", "Quiet", "Rash", "Calm","Careful", "Gentle", "Sassy", "Hasty", "Jolly", "Naive", "Timid",
 ]
 stats = ["Atk", "HP","Def","SpA","SpD"]
-'''
-HOLD
+
 input0 = input("Nickname?")
 input1 = input("Pokemon and level?")
 input1 = input1.split()
 while True:
   input2 = input("Nature?")
-if input2 in nature:
-'''
-while True: 
-  input3 = input("How was it trained?")
-  input3 = input3.split()
-  if input3[0] in stats and input3[1] in stats:
-    print("Success! ")
-'''
-    input3 = input3.split()
-
-    input4 = input("Moves learned?")
-    input4 = input4.split()
-
-'''
-#need to pull api of list of all moves? or file
-dict["Poke"] = input1[0]
-dict["Name"] = input1[1]
-dict["Lv"] = input2[1]
-dict["Ntr"] = input2[1]
-dict["Tra1"] = input3[0]
-dict["Tra2"] = input3[1]
-dict["Ab"] = input2[0]
+  if input2 in nature:
+  
+    while True: 
+      input3 = input("How was it trained?")
+      input3 = input3.split()
+      if input3[0] in stats and input3[1] in stats:
+        while True:
+          input4 = input("Moves learned?")
+          with open("list_of_moves.txt") as file:
+            contents = file.read()
+            if input4 in contents:
+              dict["Poke"] = input1[0]
+              dict["Name"] = input1[1]
+              dict["Lv"] = input2[1]
+              dict["Ntr"] = input2[1]
+              dict["Tra1"] = input3[0]
+              dict["Tra2"] = input3[1]
+              dict["Ab"] = input2[0]  
 
 print(dict)
